bc8a1ec2fe394dd1a19ac38d382a03114c9783a7
Michael Karg <mgoremeier@gmail.com>
2023-07-05 17:14:38 +0200

    UMap: make underlying Map lazy

--- libs/cardano-ledger-core/src/Cardano/Ledger/UMap.hs
+++ libs/cardano-ledger-core/src/Cardano/Ledger/UMap.hs
@@ -119,8 +119,8 @@ import Control.Monad.Trans.State.Strict (StateT (..))
 import Data.Aeson (ToJSON (..), object, (.=))
 import qualified Data.Aeson as Aeson
 import Data.Foldable (Foldable (..))
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as Map
 import Data.MapExtras (intersectDomPLeft)
 import Data.Maybe as Maybe (fromMaybe, isNothing, mapMaybe)
 import Data.Maybe.Strict (StrictMaybe (..))
